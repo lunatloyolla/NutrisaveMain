9250b55486d03a0edb8778a0d8b4bf85f9528e84
lunatloyolla <lunatloyolla@unifesspa.edu.br>
2024-09-20 19:59:11 -0300

    HTML e CSS completos

---
+++ TelaSalvos.js
@@ -0,0 +1,6 @@
+function goToTelaInicial(){
+    const botao = document.getElementById('botao');
+    if(botao){
+        window.location.href = 'Telainicial.html';
+    }
+}
---
+++ Telainicial.js
@@ -0,0 +1,9 @@
+
+
+function goToTelaSalvos() {
+    const botao = document.getElementById('botao');
+    if(botao){
+        window.location.href = 'TelaSalvos.html';
+    }
+
+};
